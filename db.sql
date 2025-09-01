05c4e78e3132de8f7894e712b3b529ede56e50ab
Breno Martins <crdj161@gmail.com>
2025-09-01 16:40:28 -0300

    Correção SQL

--- db.sql
+++ db.sql
@@ -1,3 +1,19 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           PostgreSQL 17.6 (Debian 17.6-1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 14.3.0-5) 14.3.0, 64-bit
+-- OS do Servidor:               
+-- HeidiSQL Versão:              12.11.0.7065
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES  */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Copiando estrutura para tabela public.usuarios
 CREATE TABLE IF NOT EXISTS "usuarios" (
 	"id" SERIAL NOT NULL,
 	"apelido" VARCHAR(16) NOT NULL,
@@ -8,6 +24,7 @@ CREATE TABLE IF NOT EXISTS "usuarios" (
 	UNIQUE ("apelido")
 );
 
+-- Copiando estrutura para tabela public.posts
 CREATE TABLE IF NOT EXISTS "posts" (
 	"id" SERIAL NOT NULL,
 	"autor_id" INTEGER NOT NULL,
@@ -19,7 +36,7 @@ CREATE TABLE IF NOT EXISTS "posts" (
 );
 CREATE INDEX "idx_posts_autor_id" ON "posts" ("autor_id");
 
-
+-- Copiando estrutura para tabela public.comentarios
 CREATE TABLE IF NOT EXISTS "comentarios" (
 	"id" SERIAL NOT NULL,
 	"autor_id" INTEGER NOT NULL,
@@ -33,6 +50,9 @@ CREATE TABLE IF NOT EXISTS "comentarios" (
 CREATE INDEX "idx_comentarios_post_id" ON "comentarios" ("post_id");
 CREATE INDEX "idx_comentarios_autor_id" ON "comentarios" ("autor_id");
 
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela public.mensagens
 CREATE TABLE IF NOT EXISTS "mensagens" (
 	"id" SERIAL NOT NULL,
 	"autor_id" INTEGER NOT NULL,
@@ -42,3 +62,15 @@ CREATE TABLE IF NOT EXISTS "mensagens" (
 	CONSTRAINT "mensagens_autor_id_fk" FOREIGN KEY ("autor_id") REFERENCES "usuarios" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 CREATE INDEX "idx_mensagens_autor_id" ON "mensagens" ("autor_id");
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
