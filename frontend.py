b4b3b29d81b6baac1f014300d14e8588bd1e71ad
Breno Martins <crdj161@gmail.com>
2025-10-13 22:03:09 -0300

    Arquivo de execução do front alterado

--- frontend.py
+++ frontend.py
@@ -1,7 +1,16 @@
 import http.server
 import socketserver
+from sys import argv
 
-PORT = 8000
+HOST = "localhost"
+PORT = "8080"
+
+if len(argv) > 2:
+    HOST = argv[1]
+    PORT = argv[2]
+else:
+    print(f"Usage: {argv[0]} <host> <port>")
+    exit(1) 
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     """
@@ -15,7 +24,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         return super().do_GET()
 
 Handler = MyHttpRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at http://localhost:{PORT}")
+with socketserver.TCPServer((HOST, int(PORT)), Handler) as httpd:
+    print(f"Serving at http://{HOST}:{PORT}")
     print("Blocked files: *.py, /.*")
     httpd.serve_forever()
