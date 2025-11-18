db2ae23f19bf3a24b62da7c29a5452af0c2c9a99
Breno Martins <crdj161@gmail.com>
2025-11-18 15:18:41 -0300

    Mudanças no sistema de logging no inicializador do front

--- frontend.py
+++ frontend.py
@@ -11,12 +11,17 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         starts_forbidden = any(self.path.startswith(s) for s in self.forbidden_starts)
         ends_forbidden = any(self.path.endswith(e) for e in self.forbidden_endings)
-
         if starts_forbidden or ends_forbidden:
             self.send_error(403, "Forbidden")
             return
-        
+
         return super().do_GET()
+    
+
+    def log_message(self, format, *args):
+        log_text = f"[{self.client_address[0]}:{self.client_address[1]}] [{self.log_date_time_string()}] {format % args}"
+        print(log_text)
+        open("log.txt", "a").write(log_text + "\n")
 
 if __name__ == "__main__":
     HOST = "localhost"
